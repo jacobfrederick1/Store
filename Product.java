f075c3196eb4cca9176b39d416c5a19987191547
Jacob Frederick <frederickjacob1@outlook.com>
2020-11-16 18:06:40 -0600

    remove itemId instance variable and associated methods

--- Product.java
+++ Product.java
@@ -1,34 +1,23 @@
-import jdk.javadoc.internal.doclets.formats.html.markup.HtmlAttr;
-import jdk.javadoc.internal.tool.resources.javadoc;
-
 public class Product {
-    String itemId;
     double price;
     int quantity;
 
     public Product(){
-        itemId = "Paper Towels";
         price = 5.99;
         quantity = 25;
     }
     
-    public Product(String item,int price,int quantity){
-        itemId = item;
+    public Product(int price,int quantity){
+       
         this.price = price;
         this.quantity = quantity;
     }
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
+    
     public void setPrice(double price) {
         this.price = price;
     }
     public void setQuantity(int quantity) {
         this.quantity = quantity;
-    }
-
-    public String getItemId() {
-        return itemId;
     }
     public double getPrice() {
         return price;
