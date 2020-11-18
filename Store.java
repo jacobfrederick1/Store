57fcb26204d1b43e6a94786a0824f244339d446a
Jacob Frederick <frederickjacob1@outlook.com>
2020-11-17 19:17:26 -0600

    add to process method to add and delete products from the store if already avaliable

--- Store.java
+++ Store.java
@@ -14,18 +14,40 @@ public class Store{
     }
 
 
-    public Process(String dataFile){
+    public String process(String dataFile){
         try {
             File file = new File(dataFile);
             Scanner infile = new Scanner(file);
 
-            while(infile.hasNext()){
-                System.out.print(infile.next());
-            }
+           while(infile.hasNext()){
+               String process = infile.next();
+               String item = infile.next();
+
+               if(process.equals("new")){
+                   if(!products.containsKey(item)){
+                    products.put(item, new Product(Double.parseDouble(infile.next()),Double.parseDouble(infile.next())));
+                   }
+                   else{
+                       System.out.print("Error Product already in Store");
+                   }
+               }
+               else if(process.equals("delete")){
+                        if(products.containsKey(item)){
+                            products.remove(item);
+                        }
+                        else{
+                            System.out.print("Error product could not be removed because it does not exist");
+                        }
+               }
+           }
+            
+            infile.close();
         } catch (Exception e) {
             System.out.println("Error file not found");
 
         }
+        return null;
     }
+    
 
 }
