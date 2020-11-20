6cc1f2150cbd05f85c6d542281a8654f2242a0b4
Jacob Frederick <frederickjacob1@outlook.com>
2020-11-19 18:23:19 -0600

    reformat file

--- Store.java
+++ Store.java
@@ -1,53 +1,43 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.io.*;
-import java.util.Scanner;
 
 public class Store{
     Map<String, Product> products;
- 
-    /**
-     * Default Constructor to initialize data structures
-     */
+
     public Store(){
         products = new HashMap<>();
     }
 
-
     public String process(String dataFile){
-        try {
+        try{
             File file = new File(dataFile);
             Scanner infile = new Scanner(file);
-
            while(infile.hasNext()){
                String process = infile.next();
                String item = infile.next();
 
                if(process.equals("new")){
                    if(!products.containsKey(item)){
-                    products.put(item, new Product(Double.parseDouble(infile.next()),Double.parseDouble(infile.next())));
+                    products.put(item, new Product(Double.parseDouble(infile.next()),Double.parseDouble(infile.next()),0));
                    }
                    else{
                        System.out.print("Error Product already in Store");
                    }
                }
                else if(process.equals("delete")){
-                        if(products.containsKey(item)){
-                            products.remove(item);
-                        }
-                        else{
-                            System.out.print("Error product could not be removed because it does not exist");
-                        }
+                    if(products.containsKey(item)){
+                        products.remove(item);
+                    }
+                    else{
+                        System.out.print("Error product could not be removed because it does not exist");
+                    }
                }
            }
-            
             infile.close();
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
             System.out.println("Error file not found");
-
         }
         return null;
     }
-    
-
 }
