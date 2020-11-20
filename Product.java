50c71f3f26f5a595119882d322fe4731e2e3ab3e
Jacob Frederick <frederickjacob1@outlook.com>
2020-11-19 18:22:12 -0600

    add quantity instance variable and associated methods

--- Product.java
+++ Product.java
@@ -1,16 +1,19 @@
 public class Product {
     double price;
     double sellingPrice;
+    double quantity;
 
     public Product(){
         price = 5.99;
         sellingPrice = 25;
+        quantity = 0;
     }
     
-    public Product(double price, double d){
+    public Product(double price, double sellingPrice,double quantity){
        
         this.price = price;
-        this.sellingPrice = d;
+        this.sellingPrice = sellingPrice;
+        this.quantity = quantity;
     }
     
     public void setPrice(double price) {
@@ -19,10 +22,16 @@ public class Product {
     public void setsellingPrice(int sellingPrice) {
         this.sellingPrice = sellingPrice;
     }
+    public void setQuantity(double quantity) {
+        this.quantity += quantity;
+    }
     public double getPrice() {
         return price;
     }
     public double getsellingPrice() {
         return sellingPrice;
+    }
+    public double getQuantity() {
+        return quantity;
     }
 }
